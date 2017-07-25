ad5e79733701e78ab02b38fcc4cac4e45335d3e2
Matti Vuorre <mv2521@columbia.edu>
2017-07-25 18:02:48 -0400

    Initial commit.

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Scores of Group A and Group B.
+#'
+#' A data set with the scores of two groups, Group A and Group B.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{group}{Participant's group, A or B.}
+#'   \item{score}{The participant's score in hypothetical task Z.}
+#' }
+#' @source \url{https://www.github.com/mvuorre/exampleDataPackage}
+"exampleData"
---
+++ data-raw/preprocess.R
@@ -0,0 +1,21 @@
+# This file preprocesses the data: Combine the two groups' data files,
+# and save the resulting object as a .csv and an R data file.
+
+file_type <- ".csv"
+file_path <- list.files("data-raw/", full.names = T, pattern = file_type)
+read_many <- function(files) {
+    # Function to combine many files to one data object
+    flist <- lapply(files, read.csv)
+    data_obj <- do.call(rbind, flist)
+    return(data_obj)
+}
+
+# Produce clean data by combining the two raw data files
+# The name of this object should be meaningful
+exampleData <- read_many(file_path)
+
+# Write the cleaned data to a .csv file
+write.csv(exampleData, file = "data-raw/clean_data.csv", row.names = F)
+
+# Save the cleaned data in the required R package location
+devtools::use_data(exampleData)
